e37ef8f3b094eee29428df39193054ddda8339a4
Piotr Konicki <konicki.piotr@gmail.com>
2021-06-27 20:23:23 +0200

    MyWeddiMobile - start

---
+++ app/src/main/java/com/myweddi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myweddi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
